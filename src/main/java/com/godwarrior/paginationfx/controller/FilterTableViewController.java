8344739a788acb485c8590e4365401ddcb79bde6
GodWarrior431 <jellopez1@gmail.com>
2024-06-24 21:05:40 -0300

    add Initial Connections and first functions in FilterTableView , maybe i will change the name...

--- src/main/java/com/godwarrior/paginationfx/controller/FilterTableViewController.java
+++ src/main/java/com/godwarrior/paginationfx/controller/FilterTableViewController.java
@@ -1,43 +1,65 @@
 package com.godwarrior.paginationfx.controller;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 
-public class FilterTableViewController {
+import java.lang.reflect.Field;
 
-    @FXML
-    private ImageView backPageImgView;
+public class FilterTableViewController<T> {
 
-    @FXML
-    private ImageView filterImgView;
+    private final T object;
+    private final String dataBaseName;
 
-    @FXML
-    private TableView<?> filterTableView;
+    private String query;
 
     @FXML
-    private VBox filterTableViewVBox;
+    private ImageView backPageImgView , nextPageImgView;
 
     @FXML
-    private Button goBackPageButton;
+    private ImageView filterImgView , resetFilterImgView;
 
     @FXML
-    private Button goNextButton;
+    private TableView<T> filterTableView;
 
     @FXML
-    private ImageView nextPageImgView;
+    private Button goBackPageButton, goNextPageButton;
 
     @FXML
     private TextField numberPageTextField;
 
     @FXML
-    private ComboBox<?> pageSelectComboBox;
+    private ComboBox<Integer> pageSelectComboBox;
 
-    @FXML
-    private ImageView resetFilterImgView;
+    public FilterTableViewController(T object, String dataBaseName) {
+        this.object = object;
+        this.dataBaseName = dataBaseName;
+
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public void addColumn(String columnName, String attributeName) {
+        TableColumn<T, String> column = new TableColumn<>(columnName);
+        column.setCellValueFactory(cellData -> {
+            try {
+                return new SimpleStringProperty(String.valueOf(cellData.getValue().getClass().getDeclaredField(attributeName).get(cellData.getValue())));
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                e.printStackTrace();
+                return new SimpleStringProperty("Error");
+            }
+        });
+
+        filterTableView.getColumns().add(column);
+    }
 
 }
